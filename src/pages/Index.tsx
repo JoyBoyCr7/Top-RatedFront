cf1a480f1346b0448a48d1e9fc5538adc9833d67
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-10-15 01:47:47 -0500

    styling

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,10 @@
 import { useLoaderData, Link } from "react-router-dom"
+import ShowCard from "../components/Cards"
+import * as React from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 // showName: String,  
     // yearWatched: String,
@@ -13,21 +19,39 @@ interface showtype{
     showImage : string, 
     intrestLevel : number,
     wouldRecommend: boolean,
-    Description : string, 
+    description : string, 
     userName : string,
     _id: string
 }
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
 const Index = () => {
     const shows = useLoaderData() as showtype[]
     console.log(shows)
-    return <div>
-       
-        {shows.map((show) => <div key={show._id}>
-            <h1>{show.showName}</h1>
-            <Link to={`/dashboard/${show._id}`}><div><img src={show.showImage}/></div></Link>
-        </div>)}
-    </div>
+    return <Grid container spacing={{ xs: 2, md: 1 }} columns={{ xs: 4, sm: 8, md: 12 }} display="flex"
+    justifyContent="center"
+    alignItems="center">
+            
+        {shows.map((show) => 
+            <Grid xs={3} sm={3} md={3} m={1} >
+            <Link to={`/dashboard/${show._id}`}> <ShowCard show={show}/></Link>
+            </Grid>
+        )}
+        
+    </Grid>
+   
 }
 
+// {shows.map((show) => <div key={show._id}>
+//             <h1>{show.showName}</h1>
+//             <Link to={`/dashboard/${show._id}`}><div><ShowCard show={show}/></div></Link>
+//         </div>)}
 export default Index
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,10 @@
 import { Form } from "react-router-dom"
 const Login = () => {
-    return <Form action="/login" method="post">
-        <label>Username:<input type="text" name="userName" placeholder="Username"></input></label>
-        <label>Password:<input type="password" name="password" placeholder="Password"></input></label>
-        <input type="submit" />
+    return <Form action="/login" method="post" className="form">
+        <p className="heading">Login</p>
+        <input type="text" name="userName" placeholder="Username"></input>
+        <input type="password" name="password" placeholder="Password"></input>
+        <input type="submit" className="btn"/>
     </Form>
 }
 
